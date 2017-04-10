a9d8c64aef8e69b6bc8c7ab0641056af707f4c28
Keisuke Kiriyama <krkr.xm.rkr7x@gmail.com>
2017-04-10 16:38:25 +0900

    Initial Commit

---
+++ gyro_cencer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gyro_cencer
+//
+//  Created by 桐山圭祐 on 2017/04/10.
+//  Copyright © 2017年 桐山圭祐. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
